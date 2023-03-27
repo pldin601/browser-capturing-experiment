c94ccb40917896ea10d6790250f039db3545ae39
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-03-27 18:07:02 +0100

    Cleanup

--- server2.js
+++ server2.js
@@ -1,34 +1,27 @@
 const { launch, getStream } = require("puppeteer-stream");
-const fs = require("fs");
-
-// const file = fs.createWriteStream(__dirname + "/output/test.webm");
 
 async function test() {
   const browser = await launch({
-    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
     executablePath: '/usr/bin/chromium',
     defaultViewport: {
       width: 1920,
       height: 1080,
     },
     args: [
-      // `--window-size=1920,1080`,
       `--no-sandbox`,
-      // `--kiosk`
     ]
   });
 
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 })
   await page.goto("https://soundcloud.com/platform/sama");
   const stream = await getStream(page, { audio: true, video: true });
 
   stream.pipe(process.stdout);
+
   setTimeout(async () => {
     await stream.destroy();
-    // file.close();
     await browser.close();
-  }, 1000 * 30);
+  }, 1000 * 60);
 }
 
 test();
